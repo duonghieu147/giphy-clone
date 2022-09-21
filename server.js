97dba490b02a71ed119a0b9945637ae31c81723a
Duong Hieu <duonghieu4299@gmail.com>
2022-09-21 22:40:16 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-//Install express server    
+// //Install express server    
+// const express = require('express');
+
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory    
+// app.use(express.static(__dirname + '/dist'));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port    
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
-
-const path = require('path');   
-
-const app = express();   
-
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
-
-// Start the app by listening on the default Heroku port    
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/giphy'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/giphy/index.html'));
+});
 app.listen(process.env.PORT || 8080);

87bed2f2cb3ac259071893e16c78d85e8f91b44c
Duong Hieu <duonghieu4299@gmail.com>
2022-09-21 21:20:21 +0700

    change environment

--- src/app/modules/page/gifs.service.ts
+++ src/app/modules/page/gifs.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from "rxjs/operators";
 import { Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 //i seen it use for call api gits .You can seen it at network with response GET:https://giphy.com/
 const GIPHY_FE_WEB_API_KEY = "Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g" 
@@ -44,7 +45,7 @@ export class GifsService {
    * @returns 
    */
   getTrendGifs(api_key: string = GIPHY_FE_WEB_API_KEY): Observable<any> {
-    return this.http.get<Config>("https://api.giphy.com/v1/gifs/trending?api_key="+api_key)
+    return this.http.get<Config>(environment.api+"/v1/gifs/trending?api_key="+api_key)
       .pipe(catchError(this.handleError))
   }
   
@@ -55,7 +56,7 @@ export class GifsService {
    * @returns 
    */
   getSearchGifs(api_key: string = GIPHY_FE_WEB_API_KEY,keyWord:string): Observable<any> {
-    return this.http.get<Config>("https://api.giphy.com/v1/gifs/search?offset=0&type=gifs&sort=&q="+keyWord+"&api_key="+api_key)
+    return this.http.get<Config>(environment.api+"/v1/gifs/search?offset=0&type=gifs&sort=&q="+keyWord+"&api_key="+api_key)
       .pipe(catchError(this.handleError))
   }
 
@@ -66,23 +67,23 @@ export class GifsService {
    * @returns 
    */
   getRelatedGifs(api_key: string = GIPHY_FE_WEB_API_KEY,gif_id:string): Observable<any> {
-    return this.http.get<Config>("https://api.giphy.com/v1/gifs/related?gif_id="+gif_id+"&api_key="+api_key)
+    return this.http.get<Config>(environment.api+"/v1/gifs/related?gif_id="+gif_id+"&api_key="+api_key)
       .pipe(catchError(this.handleError))
   }
 
   //TO DO
   getTrendStories(api_key: string = GIPHY_FE_STORIES_AND_GIPHY_TV_API_KEY): Observable<any> {
-    return this.http.get<Config>("https://x.giphy.com/v1/stories/trending?api_key="+api_key+"&sort=desc&gif_hydration_method=COVER_ONLY")
+    return this.http.get<Config>(environment.api1+"/v1/stories/trending?api_key="+api_key+"&sort=desc&gif_hydration_method=COVER_ONLY")
       .pipe(catchError(this.handleError))
   }
 
   getTrendVideos(api_key: string = GIPHY_FE_WEB_API_KEY): Observable<any> {
-    return this.http.get<Config>("https://api.giphy.com/v1/videos/trending?api_key="+api_key+"&offset=0")
+    return this.http.get<Config>(environment.api+"/v1/videos/trending?api_key="+api_key+"&offset=0")
       .pipe(catchError(this.handleError))
   }
 
   getGiphy(): Observable<any> {
-    return this.http.get<Config>("https://giphy.com/")
+    return this.http.get<Config>(environment.api2)
       .pipe(catchError(this.handleError))
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api:'https://api.giphy.com',
+  api1:'https://x.giphy.com',
+  api2:'https://giphy.com',
 };
